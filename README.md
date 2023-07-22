d6218d7d7c420925069aaaeda485422e8ce8805c
LUCAS MONTEIRO RIBEIRO <lucas.monteiro58@gmail.com>
2023-07-22 18:26:07 -0300

    init: 🎉

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arroxalab-site
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-screen h-screen bg-[#fbf5e9] flex items-center justify-center font-black flex-col">
+    <img src="@/assets/images/logo.jpg" alt="Logo Arroxa Lab" class="w-64" />
+    <span class="text-xl text-[#320181] font-light">Site em construção...</span>
+  </main>
+</template>
